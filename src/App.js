7b699869a1e026767988d558a2849c08b725f937
nwmaker <makernw@gmail.com>
2018-05-11 19:04:14 -0600

    add-another

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar2 from './comps/navbar2';
 import Navbar3 from './comps/navbar3';
 import Content from './comps/content';
 import CollapsableNav from './comps/collapsablenav';
+import NavMenu from './comps/navmenu';
 import logo from './logo.svg';
 import './App.css';
 
@@ -16,6 +17,7 @@ class App extends Component {
         <Navbar3 />
         <Navbar />
         <CollapsableNav />
+        <NavMenu />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React.js + FlexBox</h1>
---
+++ src/comps/navmenu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './navmenu.css'
+
+export default () => (
+  <div className='navmenu-container'>
+    <div className='navmenu'>
+      <div className='logo'>
+        <h1>NWMaker LOGO</h1>
+      </div>
+      <div className='navigation'>
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+	  <li>Blogs</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+)
