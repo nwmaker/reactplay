f6acc001f80bb69c658c1da46109291ba83d3ec1
nwmaker <makernw@gmail.com>
2018-06-08 10:16:34 -0600

    remove-navbars-in-app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import Navbar from './comps/navbar';
-import Navbar2 from './comps/navbar2';
-import Navbar3 from './comps/navbar3';
-import Content from './comps/content';
-import CollapsableNav from './comps/collapsablenav';
-import NavMenu from './comps/navmenu';
-//import Gallery from './comps/gallery';
-import Gallery from './comps/gallery2';
-import logo from './logo.svg';
-import './App.css';
+//import Navbar from './comps/navbar';
+//import Navbar2 from './comps/navbar2';
+//import Navbar3 from './comps/navbar3';
+//import Content from './comps/content';
+//import CollapsableNav from './comps/collapsablenav';
+//import NavMenu from './comps/navmenu';
+//import Gallery from './comps/gallery2';
+//import logo from './logo.svg';
+//import './App.css';
 
 import CSSAnimation from './pages/cssanimation'
 
@@ -17,20 +16,6 @@ class App extends Component {
     return (
       <div className="App">
         <CSSAnimation />
-        <hr />
-        <Navbar2 />
-        <Content />
-        <Navbar3 />
-        <Navbar />
-        <CollapsableNav />
-        <NavMenu />
-        <Gallery />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React.js + FlexBox</h1>
-        </header>
-        <p className="App-intro">
-        </p>
       </div>
     );
   }
